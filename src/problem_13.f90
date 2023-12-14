755b2e20926a4c6dd11930b8bc6b5858b7dcf171
jacobwilliams <jacobwilliams@users.noreply.github.com>
2023-12-14 00:33:07 +0000

    Deploying to gh-pages from @ jacobwilliams/AoC-2023@005cca07e9ba46d9cde0f3fd380b79e14f48c641 🚀

--- src/problem_13.f90
+++ src/problem_13.f90
@@ -21,7 +21,6 @@ open(newunit=iunit, file='inputs/day13.txt', status='OLD')
 n_lines = number_of_lines_in_file(iunit)
 isum = 0
 isum2 = 0
-allocate(lines(0))
 do iline = 1, n_lines
     line = read_line(iunit)
     if (line == '' .or. iline == n_lines) then
@@ -36,38 +35,37 @@ do iline = 1, n_lines
         end do
 
         ! part 1
-        call go(puzzle, ileft, iabove)
+        call go(puzzle,            ileft)
+        call go(transpose(puzzle), iabove)
+
         ival = ileft + 100*iabove
         isum = isum + ival
 
         ! part 2
-        ! permute each with a smudge and find the different non-zero value
         main : do m = 1, nrows
-            do n = 1, ncols
+        do n = 1, ncols
+                ! permute each with a smudge and find the different non-zero value
                 puzzle2 = puzzle
                 if (puzzle(m,n)==ASH) then
                     puzzle2(m,n) = ROCK
                 else
                     puzzle2(m,n) = ASH
                 end if
-
                 ! don't consider the one from part a (since that one still may be valid)
-                call go(puzzle2, ileft2, iabove2, ileft_skip=ileft, iabove_skip=iabove)
-
+                call go(puzzle2,            ileft2,  iskip=ileft)
+                call go(transpose(puzzle2), iabove2, iskip=iabove)
                 if ((ileft2/=ileft .or. iabove2/=iabove) .and. (ileft2/=0 .or. iabove2/=0)) then
-                    ival = ileft2 + 100*iabove2
+                    ival = ileft2 + 100*iabove2 ! found it
                     exit main
                 end if
             end do
         end do main
         isum2 = isum2 + ival
-
-        if (allocated(lines)) then
-            deallocate(lines); allocate(lines(0))
-        end if
+        deallocate(lines)
 
     else
         ! accumulate this line in the current puzzle
+        if (.not. allocated(lines)) allocate(lines(0))
         lines = [lines, string(line)]
     end if
 end do
@@ -79,31 +77,27 @@ call clk%toc('13')
 
 contains
 
-    subroutine go(puzzle, ileft, iabove, ileft_skip, iabove_skip)
+    subroutine go(puzzle, ileft, iskip)
+    !! compute the number of cols to left of mirror (0 if none)
+    !! call with transpose(puzzle) to get number of rows above.
     integer,dimension(:,:),intent(in) :: puzzle
-    integer,intent(out) :: ileft, iabove
-    integer,intent(in),optional :: ileft_skip, iabove_skip
+    integer,intent(out) :: ileft
+    integer,intent(in),optional :: iskip !! don't consider this column
 
     integer :: nrows, ncols, i, j
     logical :: mirror
 
     nrows = size(puzzle,1)
     ncols = size(puzzle,2)
-
-    ! add up the number of columns to the left of each vertical line of reflection;
-    ! to that, also add 100 multiplied by the number of rows above each horizontal
-    ! line of reflection
-
-    ! rows:
     ileft = 0
     do i = 1, ncols-1
-        if (present(ileft_skip)) then
-            if (i==ileft_skip) cycle
+        if (present(iskip)) then
+            if (i==iskip) cycle
         end if
         mirror = .true.
         do j = 1, min(i, ncols-i)
-            if (any( puzzle(:,i-j+1) /= puzzle(:,i+j)) ) then ! no mirror on this line
-                mirror = .false.
+            if (any( puzzle(:,i-j+1) /= puzzle(:,i+j)) ) then
+                mirror = .false.  ! no mirror on this line
                 exit
             end if
         end do
@@ -112,28 +106,6 @@ contains
             exit
         end if
     end do
-
-    ! rows:
-    iabove = 0
-    if (ileft==0) then
-        do i = 1, nrows-1
-            if (present(iabove_skip)) then
-                if (i==iabove_skip) cycle
-            end if
-            mirror = .true.
-            do j = 1, min(i, nrows-i)
-                if (any( puzzle(i-j+1,:) /= puzzle(i+j,:)) ) then ! no mirror on this line
-                    mirror = .false.
-                    exit
-                end if
-            end do
-            if (mirror) then
-                iabove = i
-                exit
-            end if
-        end do
-    end if
-
     end subroutine go
 
 end program problem_13
