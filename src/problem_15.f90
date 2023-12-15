1c7ff114b1e324140b6a9e31c417b307d365afb5
jacobwilliams <jacobwilliams@users.noreply.github.com>
2023-12-15 13:55:21 +0000

    Deploying to gh-pages from @ jacobwilliams/AoC-2023@952dfdee9494fda1f6a5b5cae52989a1c5699592 🚀

--- src/problem_15.f90
+++ src/problem_15.f90
@@ -7,7 +7,6 @@ implicit none
 
 integer :: i, iunit
 type(string),dimension(:),allocatable :: vals
-character(len=:),allocatable :: line
 integer(ip),dimension(:),allocatable :: ivals
 
 call clk%tic()
@@ -15,25 +14,22 @@ call clk%tic()
 ! read the data file:
 ! open(newunit=iunit, file='inputs/day15_test.txt', status='OLD')
 open(newunit=iunit, file='inputs/day15.txt', status='OLD')
-line = read_line(iunit)
-vals = split(line,',')
-allocate(ivals(size(vals))); ivals = 0
+vals = split(read_line(iunit),',')
+allocate(ivals(size(vals)))
 
 do i = 1, size(vals)
-    associate(s => vals(i)%str)
-        ivals(i) = hash(s)
-    end associate
+    ivals(i) = hash(vals(i)%str)
 end do
 write(*,*) '15a: ', sum(ivals)
 
+call clk%toc('15')
+
 contains
+
     pure elemental integer(ip) function hash(s)
+        !! Determine the ASCII code for the current character of the string.
         character(len=*),intent(in) :: s
         integer :: i
-        ! Determine the ASCII code for the current character of the string.
-        ! Increase the current value by the ASCII code you just determined.
-        ! Set the current value to itself multiplied by 17.
-        ! Set the current value to the remainder of dividing itself by 256.
         hash = 0
         do i = 1, len(s)
             hash = modulo(17_ip*( hash + iachar(s(i:i))), 256_ip)
